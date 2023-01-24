c398efb2f5e99b2411817d96fd06f17f1eb2f3d7
HarshalaUbhare <114348944+HarshalaUbhare@users.noreply.github.com>
2023-01-24 12:34:41 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,86 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+      
+    numb: 1,   
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+       "Hyper Text Preprocessor",
+       "Hyper Text Markup Language",
+       "Hyper Text Mutliple Language",
+       "Hyper Tool Multi Langauge"
+    ] 
+
+   },
+
+   {
+     
+       numb: 2,   
+       question: " Which of the following commands is used to view and edit Windows Security Policies?",
+       answer: "regedit.exe",
+       options: [
+          "regedit.exe",
+          "gpedit.msc",
+          "wf.exe",
+          "cmd"
+       ] 
+  
+      },
+
+      {
+     
+       numb: 3,   
+       question: "What is a zero-day vulnerability?",
+       answer: "A vulnerability that is known but whose fix has not yet been developed",
+       options: [
+          "A vulnerability that is known to all",
+          "vulnerability about which no one knows at all",
+          "Hyper Text Markup Language",
+          "A vulnerability that is known but whose fix has not yet been developed",
+          "None of the above"
+       ] 
+  
+      },
+
+      {
+     
+       numb: 4,   
+       question: "Which of the following should be included in the scope of penetration testing?",
+       answer: "All of the above",
+       options: [
+          "DNS server",
+          "Mail server",
+          "Communication links",
+          "All of the above"
+       ]
+      },
+
+      {
+     
+       numb: 5,   
+       question: "Which of the following is a program that helps connect to a system over the network in a secure way?",
+       answer: "Both a and b",
+       options: [
+          "SSL",
+          "Both a and b",
+          "Only a",
+          "Both a and b"
+       ] 
+  
+      },
+
+      {
+     
+       numb: 6,   
+       question: " Which of the following is a small piece of information that is sent from a website to the client system and is retained for further tracking?",
+       answer: "Cookie",
+       options: [
+           "HTTP",
+           "Cookie",
+           "XML",
+           "None of the above"
+       ] 
+  
+      }
+];
